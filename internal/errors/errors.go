2c6b3f198b3548abcd813e16981e2b1a1c95c2dc
Sasha <sashamai4@ya.ru>
2025-11-15 23:16:18 +0300

    add mapping errors to codes for HTTP responses

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -2,6 +2,7 @@ package apperrors
 
 import "errors"
 
+// Basic business logic errors
 var (
     ErrTeamExists   = errors.New("team already exists")
     ErrTeamNotFound = errors.New("team not found")
@@ -12,3 +13,38 @@ var (
     ErrNotAssigned  = errors.New("reviewer is not assigned to this PR")
     ErrNoCandidate  = errors.New("no active replacement candidate in team")
 )
+
+
+// Error codes for API responses
+type ErrorCode string
+
+
+const (
+    CodeTeamExists  ErrorCode = "TEAM_EXISTS"
+    CodePRExists    ErrorCode = "PR_EXISTS"
+    CodePRMerged    ErrorCode = "PR_MERGED"
+    CodeNotAssigned ErrorCode = "NOT_ASSIGNED"
+    CodeNoCandidate ErrorCode = "NO_CANDIDATE"
+    CodeNotFound    ErrorCode = "NOT_FOUND"
+)
+
+
+// Mapping errors to codes for HTTP responses
+func GetErrorCode(err error) ErrorCode {
+    switch {
+    case errors.Is(err, ErrTeamExists):
+        return CodeTeamExists
+    case errors.Is(err, ErrPRExists):
+        return CodePRExists
+    case errors.Is(err, ErrPRMerged):
+        return CodePRMerged
+    case errors.Is(err, ErrNotAssigned):
+        return CodeNotAssigned
+    case errors.Is(err, ErrNoCandidate):
+        return CodeNoCandidate
+    case errors.Is(err, ErrTeamNotFound), errors.Is(err, ErrUserNotFound), errors.Is(err, ErrPRNotFound):
+        return CodeNotFound
+    default:
+        return CodeNotFound
+    }
+}
